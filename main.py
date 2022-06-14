c20c54cfa7ad568940ee1e18edd9819501e89046
Saleh Zaidan <salehzaidan64@gmail.com>
2022-06-14 22:04:34 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import argparse
+import json
+import pandas as pd
+
+
+def nest(d: dict) -> dict:
+    # https://stackoverflow.com/a/50932879
+    result = {}
+    for key, value in d.items():
+        target = result
+        for k in key[:-1]:
+            target = target.setdefault(k, {})
+        target[key[-1]] = value
+    return result
+
+
+def df_to_nested_dict(df: pd.DataFrame) -> dict:
+    # https://stackoverflow.com/a/50930860
+    d = df.to_dict(orient="index")
+    return {k: nest(v) for k, v in d.items()}
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", type=argparse.FileType("r"))
+    parser.add_argument("output", type=argparse.FileType("w"))
+    args = parser.parse_args()
+
+    data = json.load(args.input)
+    df = pd.json_normalize(data, "array")
+
+    columns = ["temperature", "humidity"]
+    agg_features = ["min", "max", "median", "mean"]
+    df_agg = df.groupby("roomArea")[columns].agg(agg_features)
+
+    json.dump(df_to_nested_dict(df_agg), args.output)
+
+
+if __name__ == "__main__":
+    main()
